bde53b85c781e2aeedd87af2ab22d6405d40aa73
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2019-10-25 15:58:32 -0300

    Test
    Origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(sslRedirect());
 
 //Control de acceso
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
--- routes/markersRoute.js
+++ routes/markersRoute.js
@@ -1,4 +1,5 @@
 const markersController = require('../controllers/markersController');
+const authJwt = require('../middlewares/authJwt');
 
 module.exports = app => {
     app.post(
