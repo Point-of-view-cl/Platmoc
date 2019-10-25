aa5f83593400d2d94cad3b6410ad84bafebaf643
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2019-10-25 15:15:33 -0300

    FixBack

--- controllers/markersController.js
+++ controllers/markersController.js
@@ -1,7 +1,6 @@
 var request = require('request');
 
 async function markersList(req, res) {
-    console.log('aqui');
     try{ 
         let headers = { 
             'Content-Type': 'application/json',
